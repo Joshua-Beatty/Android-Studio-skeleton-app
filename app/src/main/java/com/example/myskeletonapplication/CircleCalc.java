e6e1af78ed9675f96d6492a5ff275990a8bd9307
Josh Beatty <joshuatb6@gmail.com>
2019-10-01 21:33:52 -0700

    finished circle calc interface and code

--- app/src/main/java/com/example/myskeletonapplication/CircleCalc.java
+++ app/src/main/java/com/example/myskeletonapplication/CircleCalc.java
@@ -1,68 +1,147 @@
 package com.example.myskeletonapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
-import android.widget.TextView;
+import java.lang.Math;
+import java.util.Locale;
 
 public class CircleCalc extends AppCompatActivity  {
 
-    double radius = 0;
-    double diameter = 0;
-    double circumference = 0;
-    double area = 0;
+    Double radius = 0d;
+    Double diameter = 0d;
+    Double circumference = 0d;
+    Double area = 0d;
+    Double pi = 3.14159d;
 
-    private EditText radiustextfield;
-    private EditText diametertextfield;
-    private EditText circumferencetextfield;
-    private EditText areatextfield;
+    private EditText radiusTextField;
+    private EditText diameterTextField;
+    private EditText circumferenceTextField;
+    private EditText areaTextField;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circle_calc);
 
-        radiustextfield = (EditText) findViewById(R.id.radiustextfield);
-        diametertextfield = (EditText) findViewById(R.id.diametertextfield);
-        circumferencetextfield = (EditText) findViewById(R.id.circumfrencetextfeild);
-        areatextfield = (EditText) findViewById(R.id.areatextfield);
+        radiusTextField = findViewById(R.id.radiusTextField);
+        diameterTextField = findViewById(R.id.diameterTextField);
+        circumferenceTextField = findViewById(R.id.circumfrencetextfeild);
+        areaTextField = findViewById(R.id.areaTextField);
 
         /* Set Text Watcher listener */
-        radiustextfield.addTextChangedListener(radiusWatcher);
+        radiusTextField.addTextChangedListener(radiusWatcher);
+        diameterTextField.addTextChangedListener(diameterWatcher);
+        circumferenceTextField.addTextChangedListener(circumferenceWatcher);
+        areaTextField.addTextChangedListener(areaWatcher);
 
 
     }
     protected void changeValues(double rad, int given){
+        radius = rad;
+        diameter = radius * 2;
+        circumference = pi * diameter;
+        area = pi * radius * radius;
+        if(area > 100000000d || circumference > 100000000d){
+            valueToLarge();
+            return;
+        }
+        radiusTextField.removeTextChangedListener(radiusWatcher);
+        diameterTextField.removeTextChangedListener(diameterWatcher);
+        circumferenceTextField.removeTextChangedListener(circumferenceWatcher);
+        areaTextField.removeTextChangedListener(areaWatcher);
 
+        radiusTextField.setHint("");
+        diameterTextField.setHint("");
+        circumferenceTextField.setHint("");
+        areaTextField.setHint("");
 
         if(given != 0){
-            diametertextfield.setText("test");
+            radiusTextField.setText(String.format(Locale.getDefault(), "%1$,.2f", radius));
         }
         if(given != 1){
-            diametertextfield.setText("test");
+            diameterTextField.setText(String.format(Locale.getDefault(), "%1$,.2f", diameter));
         }
         if(given != 2){
-            diametertextfield.setText("test");
+            circumferenceTextField.setText(String.format(Locale.getDefault(), "%1$,.2f", circumference));
         }
         if(given != 3){
-            diametertextfield.setText("test");
+            areaTextField.setText(String.format(Locale.getDefault(), "%1$,.2f", area));
         }
+        radiusTextField.addTextChangedListener(radiusWatcher);
+        diameterTextField.addTextChangedListener(diameterWatcher);
+        circumferenceTextField.addTextChangedListener(circumferenceWatcher);
+        areaTextField.addTextChangedListener(areaWatcher);
     }
-     TextWatcher radiusWatcher = new TextWatcher() {
-         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    protected void valueToLarge(){
+        radiusTextField.removeTextChangedListener(radiusWatcher);
+        diameterTextField.removeTextChangedListener(diameterWatcher);
+        circumferenceTextField.removeTextChangedListener(circumferenceWatcher);
+        areaTextField.removeTextChangedListener(areaWatcher);
 
-         }
+        radiusTextField.setHint("Too Large");
+        radiusTextField.setText("");
+        diameterTextField.setHint("Too Large");
+        diameterTextField.setText("");
+        circumferenceTextField.setHint("Too Large");
+        circumferenceTextField.setText("");
+        areaTextField.setHint("Too Large");
+        areaTextField.setText("");
 
-         public void onTextChanged(CharSequence s, int start, int before, int count) {
-             if (s.length() != 0) {
-                 changeValues(Double.toString(s), 0);
-             }
-         }
+        radiusTextField.addTextChangedListener(radiusWatcher);
+        diameterTextField.addTextChangedListener(diameterWatcher);
+        circumferenceTextField.addTextChangedListener(circumferenceWatcher);
+        areaTextField.addTextChangedListener(areaWatcher);
 
-         public void afterTextChanged(@org.jetbrains.annotations.NotNull Editable s) {
+    }
+    TextWatcher radiusWatcher = new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (s.length() != 0) {
+                changeValues(Double.parseDouble(s.toString()), 0);
+            }
         }
+
+        public void afterTextChanged(Editable s) {}
+    };
+    TextWatcher diameterWatcher = new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (s.length() != 0) {
+                changeValues(Double.parseDouble(s.toString()) * .5d, 1);
+            }
+        }
+
+        public void afterTextChanged(Editable s) {}
+    };
+    TextWatcher circumferenceWatcher = new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (s.length() != 0) {
+                changeValues(Double.parseDouble(s.toString()) /  (2 * pi), 2);
+            }
+        }
+
+        public void afterTextChanged(Editable s) {}
+    };
+    TextWatcher areaWatcher = new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (s.length() != 0) {
+                Double temp = (Math.sqrt(Double.parseDouble(s.toString()) / pi));
+                if(temp > 100000000d){
+                    valueToLarge();
+                } else {
+                    changeValues(temp, 3);
+                }
+            }
+        }
+
+        public void afterTextChanged(Editable s) {}
     };
 }
