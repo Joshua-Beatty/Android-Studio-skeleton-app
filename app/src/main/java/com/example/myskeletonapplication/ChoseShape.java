2bb3d0c46c4113c46ef218ee903e6a64b8806afa
Derek Xu <xzrderek@gmail.com>
2019-10-04 09:06:18 -0700

    finished screen

--- app/src/main/java/com/example/myskeletonapplication/ChoseShape.java
+++ app/src/main/java/com/example/myskeletonapplication/ChoseShape.java
@@ -2,7 +2,12 @@ package com.example.myskeletonapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ChoseShape extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class ChoseShape extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chose_shape);
+            final Button circleButton = findViewById(R.id.circlebutton);
+            circleButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // Code here executes on main thread after user presses button
+                    Log.d("debugging", "Clicked");
+                    startActivity(new Intent(ChoseShape.this, CircleCalc.class));
+                }
+            });
+            final Button squareButton = findViewById(R.id.squarebutton);
+            squareButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // Code here executes on main thread after user presses button
+                    Log.d("debugging", "Clicked");
+             //       startActivity(new Intent(ChoseShape.this, SquareCalc.class));
+                }
+            });
     }
 }
