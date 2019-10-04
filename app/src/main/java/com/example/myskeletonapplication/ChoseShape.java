ba6ac1a8ec6f3a2c8e967d198eb4fb100523230a
Derek Xu <xzrderek@gmail.com>
2019-10-04 09:33:00 -0700

    javadocs

--- app/src/main/java/com/example/myskeletonapplication/ChoseShape.java
+++ app/src/main/java/com/example/myskeletonapplication/ChoseShape.java
@@ -7,14 +7,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+
+/**
+ * ChoseShape class is a screen that allows the user to choose between a circle and a square to calculate.
+ *
+ * @author Derek Xu
+ * @version 10/4/19
+ */
 
 public class ChoseShape extends AppCompatActivity {
 
     @Override
+
+    /**
+     * onCreate method makes the buttons clickable to change screens
+     * changes title on top to No More Geometry
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chose_shape);
+        getSupportActionBar().setTitle("No More Geometry");
             final Button circleButton = findViewById(R.id.circlebutton);
             circleButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
